da6c50f32832a7ebecd189ec066ac244e430463e
Ripazhakgggdkp <ripazhakgggdkp@users.noreply.github.com>
2022-09-26 21:08:01 -0500

    chore: fix some stolen code

--- components/issues.tsx
+++ components/issues.tsx
@@ -44,17 +44,23 @@ const invertColor = (hex: string, bw = true) => {
   if (hex.length !== 6) {
     throw new Error("Invalid HEX color.");
   }
-  var r = parseInt(hex.slice(0, 2), 16),
-    g = parseInt(hex.slice(2, 4), 16),
-    b = parseInt(hex.slice(4, 6), 16);
+  let r = parseInt(hex.slice(0, 2), 16);
+  let g = parseInt(hex.slice(2, 4), 16);
+  let b = parseInt(hex.slice(4, 6), 16);
   if (bw) {
     // https://stackoverflow.com/a/3943023/112731
     return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#FFFFFF";
   }
   // invert color components
-  r = (255 - r).toString(16);
-  g = (255 - g).toString(16);
-  b = (255 - b).toString(16);
+  const ir = (255 - r).toString(16);
+  const ig = (255 - g).toString(16);
+  const ib = (255 - b).toString(16);
   // pad each with zeros and return
-  return "#" + padZero(r) + padZero(g) + padZero(b);
+  return "#" + padZero(ir) + padZero(ig) + padZero(ib);
+};
+
+const padZero = (str, len = 2) => {
+  len = len || 2;
+  var zeros = new Array(len).join("0");
+  return (zeros + str).slice(-len);
 };
